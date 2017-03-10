ed13c149832cc755e1b01111305191cad01d550d
Mischa Beumer <mischabeumer@Mischas-MacBook-Pro.local>
2017-03-10 11:07:48 -0500

    No changes - testing source control

---
+++ Homepwner/ItemStore.swift
@@ -0,0 +1,27 @@
+//
+//  ItemStore.swift
+//  Homepwner
+//
+//  Created by Mischa Beumer on 3/10/17.
+//  Copyright © 2017 msbbeumer. All rights reserved.
+//
+
+import UIKit
+
+class ItemStore {
+    var allItems = [Item]()
+    
+    @discardableResult func createItem() -> Item {
+        let newItem = (Item(random: true))
+        
+        allItems.append(newItem)
+        
+        return newItem
+    }
+    
+    init () {
+        for _ in 0..<5 {
+            createItem()
+        }
+    }
+}
